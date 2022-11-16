51cf83604a79a7ef76e6054526adc728fbf5989e
adrien lalut <adrien.lalut@hotmail.fr>
2022-11-16 16:57:30 +0100

    Yelp MVC - Finished but not completly styled

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/show'
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'reviews/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root 'restaurants#index'
+  resources :restaurants, only: %i[index new create show] do
+    resources :reviews, only: %i[index create]
+  end
+  # resources :reviews, only: %i[show edit update destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Faker::Config.locale = 'fr'
+arr = %w[chinese italian japanese french belgian]
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: arr.sample
+  )
+  puts "Created #{restaurant.name}"
+  restaurant.save!
+end
+
+puts 'Finished!'
